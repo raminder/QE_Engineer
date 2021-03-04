3deba28191b815b29bdb62451d1918c96d7dd72d
Raminder <kaur.raminder811@gmail.com>
2021-03-04 13:07:28 -0500

    Intial project structure

---
+++ src/main/java/dev/swapi/SwapiAPIFramework/App.java
@@ -0,0 +1,13 @@
+package dev.swapi.SwapiAPIFramework;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello!" );
+    }
+}
